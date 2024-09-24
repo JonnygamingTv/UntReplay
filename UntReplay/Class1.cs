f0891bd69443cb1cc766b751287dca08609392ca
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-09-24 17:05:05 +0200

    Add files via upload

---
+++ UntReplay/Class1.cs
@@ -0,0 +1,143 @@
+﻿using SDG.Framework.Modules;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UntReplay
+{
+    public class Class1 : SDG.Unturned.BarricadeManager, IModuleNexus
+    {
+        public void initialize()
+        {
+            Console.WriteLine("Unturned ReplayMod starting..\nBrought to you by JonHosting.com!");
+            Console.WriteLine("Adding level listeners..");
+            /* Listen on level load to reset data, basically. */
+            SDG.Unturned.Level.onPostLevelLoaded += OLL;
+            SDG.Unturned.Level.onLevelExited += OLE;
+            Console.WriteLine("Adding barricade listeners..");
+            /* Barricades */
+            // Add when placed
+            SDG.Unturned.BarricadeManager.onBarricadeSpawned += oBS;
+            SDG.Unturned.BarricadeManager.onDeployBarricadeRequested += ODBS;
+            // Modify when changed
+            SDG.Unturned.BarricadeManager.onTransformRequested += oTR;
+            // Remove when destroyed
+            SDG.Unturned.BarricadeManager.onDamageBarricadeRequested += oDBR;
+            SDG.Unturned.BarricadeDrop.OnSalvageRequested_Global += oSR;
+            Console.WriteLine("Adding structure listeners..");
+            /* Structures */
+            // Add when placed
+            SDG.Unturned.StructureManager.onStructureSpawned += oSS;
+            SDG.Unturned.StructureManager.onDeployStructureRequested += ODSS;
+            // Modify when changed
+            SDG.Unturned.StructureManager.onTransformRequested += oTR;
+            // Remove when destroyed
+            SDG.Unturned.StructureManager.onDamageStructureRequested += oDSR;
+            SDG.Unturned.StructureDrop.OnSalvageRequested_Global += oSR;
+            Console.WriteLine("Adding player listeners..");
+            /* Players */
+            // Add on join
+            SDG.Unturned.Player.onPlayerCreated += oPC;
+            // Modify when moving or gesturing/stance
+
+            SDG.Unturned.PlayerAnimator.OnGestureChanged_Global += POGC;
+            SDG.Unturned.PlayerStance.OnStanceChanged_Global += POSC;
+            SDG.Unturned.PlayerEquipment.OnPunch_Global += POP;
+            // SDG.Unturned.PlayerClothing.OnBackpackChanged_Global
+
+            // Keep track of death
+            SDG.Unturned.PlayerLife.onPlayerDied += oPD;
+            // Remove on leave
+            
+
+            Console.WriteLine("Adding Animal listeners..");
+            /* Animals */
+
+            // Keep track of death
+
+            Console.WriteLine("Adding zombie listeners..");
+            /* Zombies */
+
+        }
+
+        public void shutdown()
+        {
+            Console.WriteLine("Unturned ReplayMod exiting..");
+        }
+        // Level handlers
+        void OLL(int level) { }
+        void OLE() { }
+
+        // Barricade/structure spawns
+        void oBS(SDG.Unturned.BarricadeRegion region, SDG.Unturned.BarricadeDrop drop)
+        {
+
+        }
+        void oSS(SDG.Unturned.StructureRegion region, SDG.Unturned.StructureDrop drop)
+        {
+
+        }
+        // Deploy handlers, basically spawn handlers
+        void ODBS(SDG.Unturned.Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
+        {
+
+        }
+        void ODSS(Structure structure, ItemStructureAsset asset, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
+        {
+        }
+        // Move handlers
+        void oTR(CSteamID instigator, byte x, byte y, ushort plant, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow) // Barricade
+        {
+
+        }
+        void oTR(CSteamID instigator, byte x, byte y, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow) // Structure
+        {
+
+        }
+
+        // Salvage handlers
+        void oSR(SDG.Unturned.BarricadeDrop structure, SDG.Unturned.SteamPlayer instigatorClient, ref bool shouldAllow)
+        {
+
+        }
+        void oSR(SDG.Unturned.StructureDrop structure, SDG.Unturned.SteamPlayer instigatorClient, ref bool shouldAllow)
+        {
+
+        }
+        // Damage handlers
+        void oDBR(CSteamID instigatorSteamID, Transform barricadeTransform, ref ushort pendingTotalDamage, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+
+        }
+        void oDSR(CSteamID instigatorSteamID, Transform structureTransform, ref ushort pendingTotalDamage, ref bool shouldAllow, EDamageOrigin damageOrigin)
+        {
+
+        }
+        // Player handlers
+        void oPC(Player player) // Create
+        {
+
+        }
+
+        void oPD(PlayerLife sender, EDeathCause cause, ELimb limb, CSteamID instigator) // Death
+        {
+        }
+        void POGC(PlayerAnimator a, EPlayerGesture b)
+        {
+
+        }
+        void POSC(PlayerStance a)
+        {
+
+        }
+        void POP(PlayerEquipment a, EPlayerPunch b)
+        {
+
+        }
+    }
+}
---
+++ UntReplay/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UntReplay")]
+[assembly: AssemblyDescription("Store changes happening in your world to later render custom scenes. Inspired by the Minecraft Replaymod mod.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JonHosting")]
+[assembly: AssemblyProduct("UntReplay")]
+[assembly: AssemblyCopyright("Copyright © JonHosting 2024")]
+[assembly: AssemblyTrademark("Jon")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7c9b1c2-33ba-4745-88f0-1cfc1172b086")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
