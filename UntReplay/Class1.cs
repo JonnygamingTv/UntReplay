af839e6b55c637e467e014d60b69dc7af5a866e7
Jonnygaming Tv <22498524+JonnygamingTv@users.noreply.github.com>
2024-09-24 18:44:56 +0200

    Add files via upload

--- UntReplay/Class1.cs
+++ UntReplay/Class1.cs
@@ -12,46 +12,47 @@ namespace UntReplay
 {
     public class Class1 : SDG.Unturned.BarricadeManager, IModuleNexus
     {
+        List<String> Logs;
         public void initialize()
         {
             Console.WriteLine("Unturned ReplayMod starting..\nBrought to you by JonHosting.com!");
             Console.WriteLine("Adding level listeners..");
             /* Listen on level load to reset data, basically. */
-            SDG.Unturned.Level.onPostLevelLoaded += OLL;
-            SDG.Unturned.Level.onLevelExited += OLE;
+            SDG.Unturned.Level.onPostLevelLoaded += OLL; // 1
+            SDG.Unturned.Level.onLevelExited += OLE; // 2
             Console.WriteLine("Adding barricade listeners..");
             /* Barricades */
             // Add when placed
-            SDG.Unturned.BarricadeManager.onBarricadeSpawned += oBS;
-            SDG.Unturned.BarricadeManager.onDeployBarricadeRequested += ODBS;
+            SDG.Unturned.BarricadeManager.onBarricadeSpawned += oBS; // 3
+            SDG.Unturned.BarricadeManager.onDeployBarricadeRequested += ODBS; // 4
             // Modify when changed
-            SDG.Unturned.BarricadeManager.onTransformRequested += oTR;
+            SDG.Unturned.BarricadeManager.onTransformRequested += oTR; // 5
             // Remove when destroyed
-            SDG.Unturned.BarricadeManager.onDamageBarricadeRequested += oDBR;
-            SDG.Unturned.BarricadeDrop.OnSalvageRequested_Global += oSR;
+            SDG.Unturned.BarricadeManager.onDamageBarricadeRequested += oDBR; // 6
+            SDG.Unturned.BarricadeDrop.OnSalvageRequested_Global += oSR; // 7
             Console.WriteLine("Adding structure listeners..");
             /* Structures */
             // Add when placed
-            SDG.Unturned.StructureManager.onStructureSpawned += oSS;
-            SDG.Unturned.StructureManager.onDeployStructureRequested += ODSS;
+            SDG.Unturned.StructureManager.onStructureSpawned += oSS; // 8
+            SDG.Unturned.StructureManager.onDeployStructureRequested += ODSS; // 9
             // Modify when changed
-            SDG.Unturned.StructureManager.onTransformRequested += oTR;
+            SDG.Unturned.StructureManager.onTransformRequested += oTR; // 10
             // Remove when destroyed
-            SDG.Unturned.StructureManager.onDamageStructureRequested += oDSR;
-            SDG.Unturned.StructureDrop.OnSalvageRequested_Global += oSR;
+            SDG.Unturned.StructureManager.onDamageStructureRequested += oDSR; // 11
+            SDG.Unturned.StructureDrop.OnSalvageRequested_Global += oSR; // 12
             Console.WriteLine("Adding player listeners..");
             /* Players */
             // Add on join
-            SDG.Unturned.Player.onPlayerCreated += oPC;
+            SDG.Unturned.Player.onPlayerCreated += oPC; // 13
             // Modify when moving or gesturing/stance
 
-            SDG.Unturned.PlayerAnimator.OnGestureChanged_Global += POGC;
-            SDG.Unturned.PlayerStance.OnStanceChanged_Global += POSC;
-            SDG.Unturned.PlayerEquipment.OnPunch_Global += POP;
+            SDG.Unturned.PlayerAnimator.OnGestureChanged_Global += POGC; // 14
+            SDG.Unturned.PlayerStance.OnStanceChanged_Global += POSC; // 15
+            SDG.Unturned.PlayerEquipment.OnPunch_Global += POP; // 16
             // SDG.Unturned.PlayerClothing.OnBackpackChanged_Global
 
             // Keep track of death
-            SDG.Unturned.PlayerLife.onPlayerDied += oPD;
+            SDG.Unturned.PlayerLife.onPlayerDied += oPD; // 17
             // Remove on leave
             
 
@@ -65,11 +66,11 @@ namespace UntReplay
 
             Console.WriteLine("Adding vehicle listeners..");
             /* Vehicles */
-            SDG.Unturned.VehicleManager.onEnterVehicleRequested += OEV;
-            SDG.Unturned.VehicleManager.onExitVehicleRequested += OExV;
-            VehicleManager.onSwapSeatRequested += OSSR;
-            SDG.Unturned.VehicleManager.onDamageTireRequested += ODT;
-            SDG.Unturned.VehicleManager.OnVehicleExploded += OVE;
+            SDG.Unturned.VehicleManager.onEnterVehicleRequested += OEV; // 18
+            SDG.Unturned.VehicleManager.onExitVehicleRequested += OExV; // 19
+            VehicleManager.onSwapSeatRequested += OSSR; // 20
+            SDG.Unturned.VehicleManager.onDamageTireRequested += ODT; // 21
+            SDG.Unturned.VehicleManager.OnVehicleExploded += OVE; // 22
         }
 
         public void shutdown()
@@ -77,35 +78,48 @@ namespace UntReplay
             Console.WriteLine("Unturned ReplayMod exiting..");
         }
         // Level handlers
-        void OLL(int level) { }
-        void OLE() { }
+        void OLL(int level) {
+            Logs = new List<String>();
+            foreach (InteractableVehicle Ve in SDG.Unturned.VehicleManager.vehicles)
+                Logs.Add("01"+Ve.instanceID.ToString()+'|'+Ve.asset.GUID+'|'+Ve.transform.position.x+','+Ve.transform.position.y+','+Ve.transform.position.z+','+Ve.transform.rotation.w+','+Ve.transform.rotation.x+','+Ve.transform.rotation.y+','+Ve.transform.rotation.z+'|'+Ve.tires.ToArray().ToString());
+        }
+        void OLE() {
+            System.IO.File.WriteAllLines(UnturnedPaths.RootDirectory.FullName+"/UntReplay.log", Logs);
+        }
 
         // Barricade/structure spawns
         void oBS(SDG.Unturned.BarricadeRegion region, SDG.Unturned.BarricadeDrop drop)
         {
-
+            Logs.Add("03"+drop.instanceID+"|"+drop.asset.GUID+"|"+drop.model.position.x+","+drop.model.position.y+","+drop.model.position.z+","+drop.model.rotation.w+","+drop.model.rotation.x+","+drop.model.rotation.y+","+drop.model.rotation.z);
         }
         void oSS(SDG.Unturned.StructureRegion region, SDG.Unturned.StructureDrop drop)
         {
-
+            Logs.Add("04"+drop.instanceID+"|"+drop.asset.GUID+"|"+drop.model.position.x+","+drop.model.position.y+","+drop.model.position.z+","+drop.model.rotation.w+","+drop.model.rotation.x+","+drop.model.rotation.y+","+drop.model.rotation.z);
         }
         // Deploy handlers, basically spawn handlers
         void ODBS(SDG.Unturned.Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
         {
-
-        }
+            
+        } // 4
         void ODSS(Structure structure, ItemStructureAsset asset, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldAllow)
         {
-        }
+
+        } // 9
         // Move handlers
         void oTR(CSteamID instigator, byte x, byte y, ushort plant, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow) // Barricade
         {
-
-        }
+            BarricadeDrop drop;
+            if (BarricadeManager.tryGetRegion(x, y, plant, out BarricadeRegion barricadeRegion) && (drop = barricadeRegion.drops.Find((BarricadeDrop o) => o.instanceID == instanceID)) != null) {
+                Logs.Add("05" + instanceID + "|" +drop.asset.GUID + "|" + drop.model.position.x + "," + drop.model.position.y + "," + drop.model.position.z + "," + drop.model.rotation.w + "," + drop.model.rotation.x + "," + drop.model.rotation.y + "," + drop.model.rotation.z);
+            }
+        } // 5
         void oTR(CSteamID instigator, byte x, byte y, uint instanceID, ref Vector3 point, ref byte angle_x, ref byte angle_y, ref byte angle_z, ref bool shouldAllow) // Structure
         {
-
-        }
+            StructureDrop drop;
+            if (StructureManager.tryGetRegion(x, y, out StructureRegion structureRegion) && (drop = structureRegion.drops.Find((StructureDrop o) => o.instanceID == instanceID)) != null) {
+                Logs.Add("10" + instanceID + "|" + drop.asset.GUID + "|" + drop.model.position.x + "," + drop.model.position.y + "," + drop.model.position.z + "," + drop.model.rotation.w + "," + drop.model.rotation.x + "," + drop.model.rotation.y + "," + drop.model.rotation.z);
+            }
+        } // 10
 
         // Salvage handlers
         void oSR(SDG.Unturned.BarricadeDrop structure, SDG.Unturned.SteamPlayer instigatorClient, ref bool shouldAllow)
@@ -148,7 +162,9 @@ namespace UntReplay
         }
         // Vehicles
         void OSSR(Player player, InteractableVehicle vehicle, ref bool shouldAllow, byte fromSeatIndex, ref byte toSeatIndex) { } // Swap seat
-        void OEV(Player player, InteractableVehicle vehicle, ref bool shouldAllow) { } // Enter veh
+        void OEV(Player player, InteractableVehicle vehicle, ref bool shouldAllow) {
+            Logs.Add("18"+vehicle.instanceID+'|');
+        } // Enter veh
         void OExV(Player player, InteractableVehicle vehicle, ref bool shouldAllow, ref Vector3 pendingLocation, ref float pendingYaw) { } // Exit veh
         void ODT(CSteamID instigatorSteamID, InteractableVehicle vehicle, int tireIndex, ref bool shouldAllow, EDamageOrigin damageOrigin) { }
         void OVE(InteractableVehicle Veh) { }
